d8736328a992d6d75c3db3f70026c3643be8a87b
Marlon Knupp <marlonjcc23@gmail.com>
2024-07-14 08:40:38 -0300

    alteraçoes

--- SQL.py
+++ SQL.py
@@ -1,32 +1,27 @@
 import sqlite3
 
-conexao = sqlite3.connect ("Meu_banco.sqlite")
+conexao = sqlite3.connect("Meu_banco.sqlite")
 cursor = conexao.cursor()
-
-# criando tabela
 
 def criar_tabela (cursor):
     cursor.execute('CREATE TABLE Clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR (100), email VARCHAR (150))')
 
     conexao.commit ()
-
-# inserindo registros 
+ 
 
 def inserir_registro (conexao, cursor, nome, email):
     data = (nome, email)
     cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
     conexao.commit() 
 
-# atualizando registro
 
 def atualizar_registro(conexao, cursor, nome, email, id):
     data = (nome, email, id)
     cursor.execute("UPDATE clientes SET nome=?, email=? WHERE id=?;", data)
     conexao.commit()
 
-# inserindo registros em lote 
 
-def inserir_muitos (conexao, cursor, dados):
+def inserir_lotes (conexao, cursor, dados):
     cursor.executemany(" INSERT INTO clientes (nome, email) VALUES (?,?)",dados)
     conexao.commit()
 
@@ -35,4 +30,4 @@ dados = [
     ("rodrigo", "rodrigo@gmail.com"),
     ("maria", "maria@gmail.com"),
 ]
-inserir_muitos(conexao, cursor, dados)
+inserir_lotes(conexao, cursor, dados)
